af646eea840a0aa4448c7f6c748661f8ae7302e4
Jon <jon@epoch.(none)>
2013-03-07 13:50:19 -0600

    Clarified examples

--- blueprints/helloworld.py
+++ blueprints/helloworld.py
@@ -1,10 +1,17 @@
 # Easiest API module ever made in the world.
 from flask import Blueprint
+
 module = Blueprint('helloworld', __name__)
+
 @module.route('/hello', methods=['GET'])
-def testone():
-    return "Hello, world.\n"
+def helloworld():
+    """ Example of validating request data and formulating customized response. """
+    from flask import request   # Required to access request.method etc
+    from flask import Response  # Lets us get fancy with Responses.
+    if request.method == 'GET':
+        return Response("Hello, world.\n",200,mimetype='application/json')
 
 @module.route('/hello/<world>', methods=['GET'])
-def testtwo(world):
+def hello(world):
+    """ Simple example, using <world> as an object """
     return "Hello, " + str(world) + ".\n" 
